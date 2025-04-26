f6bf73066d535cbb5dab66ade5fe1ac27da4adfc
Alex Nunes <alex.nunes2821@outlook.com>
2025-04-25 22:11:01 -0300

    Cria conexão com banco de dados

---
+++ sistema-gerenciamento-pedidos/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace sistema_gerenciamento_pedidos.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
